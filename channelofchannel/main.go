e7353aef0bdb3249b195150b8da25fe28dca4845
mukulrawat1986 <mukulsrawat@gmail.com>
2015-09-23 19:49:54 +0530

    slight modifications

--- channelofchannel/main.go
+++ channelofchannel/main.go
@@ -1,54 +1,54 @@
 package main
 
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
 )
 
 // We can use a channel of channel to send to the receive the channel on which the data will be sent
 // We are also using a timeout over here, so the data emission will stop after 3 seconds
-func emit(chanChannel chan chan string, doneCh chan bool){
+func emit(chanChannel chan chan string, doneCh chan bool) {
 
-    // Create a channel of string and send it to our channel of channel
-    wordChannel := make(chan string)
-    chanChannel <- wordChannel
+	// Create a channel of string and send it to our channel of channel
+	wordChannel := make(chan string)
+	chanChannel <- wordChannel
 
-    defer close(wordChannel)
+	defer close(wordChannel)
 
-    words := []string{"the", "quick", "brown", "fox"}
+	words := []string{"the", "quick", "brown", "fox"}
 
-    i := 0
+	i := 0
 
-    t := time.NewTimer(1 * time.Second)
+	t := time.NewTimer(1 * time.Second)
 
-    for {
-        select {
-        case wordChannel <- words[i]:
-            i += 1
-            if i == len(words){
-                i = 0
-            }
+	for {
+		select {
+		case wordChannel <- words[i]:
+			i += 1
+			if i == len(words) {
+				i = 0
+			}
 
-        case <- doneCh:
-            doneCh <- true
-            return
+		case <-doneCh:
+			doneCh <- true
+			return
 
-        case <- t.C:
-            return
-        }
-    }
+		case <-t.C:
+			return
+		}
+	}
 }
 
 func main() {
 
-    channelCh := make(chan chan string)
-    doneCh := make(chan bool)
+	channelCh := make(chan chan string)
+	doneCh := make(chan bool)
 
-    go emit(channelCh, doneCh)
+	go emit(channelCh, doneCh)
 
-    wordCh := <- channelCh
+	wordCh := <-channelCh
 
-    for word := range wordCh {
-        fmt.Printf("%s\n", word)
-    }
+	for word := range wordCh {
+		fmt.Printf("%s\n", word)
+	}
 }
