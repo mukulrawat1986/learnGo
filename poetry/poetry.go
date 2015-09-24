465fb68cd3ad0afedba559d2601ee2377f0f81de
mukulrawat1986 <mukulsrawat@gmail.com>
2015-09-24 05:51:38 +0530

    added functionality to load a poem from a file

--- poetry/poetry.go
+++ poetry/poetry.go
@@ -1,7 +1,10 @@
 package poetry
 
 import (
+    "bufio"
     "fmt"
+    _"io"
+    "os"
 )
 
 // Create some types of our own
@@ -9,10 +12,48 @@ type Line string
 type Stanza []Line
 type Poem []Stanza
 
-// Create our poem
+// Create an empty poem
 func NewPoem() Poem{
     return Poem{}
 }
+
+// Load a poem from a file
+func LoadPoem(filename string) (Poem, error){ 
+    f, err := os.Open(filename)
+    if err != nil{
+        return nil, err
+    }
+    defer f.Close()
+
+    p := Poem{}
+
+    var s Stanza
+
+    scan := bufio.NewScanner(f)
+
+    for scan.Scan(){
+        l := scan.Text()
+
+        // If there is nothing in the line, that means Stanza is finished, so append it to the poem
+        // If not then append the line to Stanza after converting the string to a Line
+        if l == ""{
+            p = append(p, s)
+            s = Stanza{}
+            continue
+        }
+        s = append(s, Line(l))
+    }
+    // At the end of the scanning we will add the last token which will be emptry String since the Stanza finished
+    p = append(p, s)
+
+    if scan.Err() != nil{
+        return nil, scan.Err()
+    }
+
+    return p, nil
+}
+
+
 
 // Get the number of Stanzas in a poem
 func (p Poem) NumStanzas() int{
